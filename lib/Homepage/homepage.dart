4aaeb551eee722621bd8e130daa6bba2252dcb44
Pak_Man926 <davidkamau926@gmail.com>
2025-04-03 15:26:59 +0300

    Homepage Update

--- lib/Homepage/homepage.dart
+++ lib/Homepage/homepage.dart
@@ -1,49 +1,77 @@
 import "package:flutter/material.dart";
 import "package:lg_tribe/Login/login.dart";
+import "package:lg_tribe/States/bottomnavigation.dart";
 
-class Homepage extends StatelessWidget
-{
+class Homepage extends StatefulWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  final List<Widget> _children = [
+    Center(child: Text("Home")),
+    Center(child: Text("Upload")),
+    Center(child: Text("Notifications")),
+    Center(child: Text("Profile")),
+  ];
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leading: Icon(
-            Icons.arrow_back,
-            color: Colors.blueGrey
-          ),
+          leading: Icon(Icons.arrow_back, color: Colors.blueGrey),
           centerTitle: true,
-          title: Text("LG Tr!be", style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
+          title: Text(
+            "LG Tr!be",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+          ),
           backgroundColor: const Color.fromARGB(255, 245, 244, 242),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home_rounded),
-              label: "Home",
-              backgroundColor: Color(0xFFA50034),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.add_box_rounded),
-              label: "Upload",
-              backgroundColor: Color(0xFFA50034),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.notification_add_rounded),
-              label: "Notifications",
-              backgroundColor: Color(0xFFA50034),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_2_rounded),
-              label: "Profile",
-              backgroundColor: Color(0xFFA50034),
-            )
-          ],
-        ) ,
-      )
+        body: _children[_selectedIndex],
+        bottomNavigationBar: bottomNavigator(),
+      ),
+    );
+  }
+
+  BottomNavigationBar bottomNavigator() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+      backgroundColor: Color(0xFFA50034),
+      selectedItemColor: Colors.white, // Adjust this based on your UI
+      unselectedItemColor: Colors.grey,
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home_rounded),
+          label: "Home",
+          //backgroundColor: Color(0xFFA50034),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_rounded),
+          label: "Upload",
+          //backgroundColor: Color(0xFFA50034),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notification_add_rounded),
+          label: "Notifications",
+          //backgroundColor: Color(0xFFA50034),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_rounded),
+          label: "Profile",
+          //backgroundColor: Color(0xFFA50034),
+        ),
+      ],
     );
   }
 }
-
--- lib/States/bottomnavigation.dart
+++ lib/States/bottomnavigation.dart
@@ -1,18 +1,44 @@
-// import "package:flutter/material.dart";
+import "package:flutter/material.dart";
 
-// class BottomNavigation extends StatefulWidget
-// {
-//   const BottomNavigation({super.key});
+class BottomNavigation extends StatefulWidget {
+  final Function(int) onItemTapped;
+  final int selectedIndex;
 
-//   State<BottomNavigationBar> createState() => _BottomNavigationBarState();
+  const BottomNavigation({
+    Key? key,
+    required this.onItemTapped,
+    required this.selectedIndex,
+  }) : super(key: key);
 
-// }
+  @override
+  State<BottomNavigation> createState() => _BottomNavigationState();
+}
 
-// class _BottomNavigationBarState extends State<BottomNavigationBar>
-// {
-//   int _selectedIndex = 0;
-
-//   static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-
-
-// }
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: const Color(0xFFA50034),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      currentIndex: widget.selectedIndex,
+      onTap: widget.onItemTapped,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_rounded), label: "Home"),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_box_rounded),
+          label: "Upload",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notification_add_rounded),
+          label: "Notifications",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_2_rounded),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+}
