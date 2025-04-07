24f71c599aff00b635deaf9e69aa4e30cd16fef4
Pak_Man926 <davidkamau926@gmail.com>
2025-04-08 00:01:08 +0300

    Homepage 6.2

--- lib/Homepage/Screens/homescreen.dart
+++ lib/Homepage/Screens/homescreen.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import "package:lg_tribe/Homepage/Screens/models/homescreen_model.dart";
 
-class Homescreen extends StatelessWidget {
+class Homescreen extends StatefulWidget {
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  bool isReadMore = false;
+
   List<Post> posts = [
     Post(
       username: "Esther Wanjiru",
       userImageUrl: "https://picsum.photos/250?image=9",
       timestamp: "1hr ago",
       contentText:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       contentImageUrl: "https://picsum.photos/id/27/367/267",
     ),
   ];
@@ -17,62 +24,95 @@ class Homescreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          // Profile section
           Row(
             children: [
-              //Profile Picture
               Container(
                 alignment: Alignment.center,
                 margin: EdgeInsets.only(left: 10, top: 10),
-                //Padding(padding: edgeInsets.all(10),),
                 child: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    "https://picsum.photos/250?image=9",
-                  ),
+                  backgroundImage: NetworkImage(posts[0].userImageUrl),
                   radius: 30,
                 ),
               ),
               SizedBox(width: 10),
-              //Username 
               Text(
-                "Esther Wanjiru",
+                posts[0].username,
                 style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
               ),
               SizedBox(width: 10),
-              //Timestamp
               Text(
-                "1h ago",
+                posts[0].timestamp,
                 style: TextStyle(fontSize: 10, fontWeight: FontWeight.w200),
               ),
             ],
           ),
           SizedBox(height: 5),
-          //Post Content
+
+          // Post image
           Container(
-            height: 400,
-            color: Colors.grey[300],
-          ),
-          //Below the post => Post caption, like and dislike buttons
-          Row(children: [
-            Padding(padding: EdgeInsets.only(top:8, left:5),
-            child: RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                    style: TextStyle(
-                      fontSize: 10,
-                      fontWeight: FontWeight.w200,
-                      color: Colors.black,
-                    ),
-                  )
-                ]
+            height: 200,
+            margin: EdgeInsets.symmetric(vertical: 10),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(posts[0].contentImageUrl),
+                fit: BoxFit.cover,
               ),
             ),
-            ),
-            Icon(Icons.arrow_upward),
-            Icon(Icons.arrow_downward),
-          ],)
+          ),
+
+          // Post caption and buttons
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        posts[0].contentText,
+                        style: TextStyle(
+                          fontSize: 10,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.black,
+                        ),
+                        maxLines: isReadMore ? null : 2,
+                        overflow:
+                            isReadMore
+                                ? TextOverflow.visible
+                                : TextOverflow.ellipsis,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isReadMore = !isReadMore;
+                          });
+                        },
+                        child: Text(
+                          isReadMore ? 'Read less' : 'Read more',
+                          style: TextStyle(
+                            fontSize: 10,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Icon(Icons.arrow_upward),
+                  Icon(Icons.arrow_downward),
+                ],
+              ),
+            ],
+          ),
         ],
       ),
     );
