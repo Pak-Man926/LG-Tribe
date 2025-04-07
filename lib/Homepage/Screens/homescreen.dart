d4b71cfe9f345768a3fb591c06d640cb90af0643
Pak_Man926 <davidkamau926@gmail.com>
2025-04-07 17:22:24 +0300

    Homepage 6.1

--- lib/Homepage/Screens/homescreen.dart
+++ lib/Homepage/Screens/homescreen.dart
@@ -16,52 +16,64 @@ class Homescreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: posts.length,
-        itemBuilder: (Context, index) {
-          return Card(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      alignment: Alignment.center,
-                      margin: EdgeInsets.only(left: 10, top: 10),
-                      //Padding(padding: edgeInsets.all(10),),
-                      child: CircleAvatar(
-                        backgroundImage: NetworkImage(
-                          posts[index].userImageUrl,
-                        ),
-                        radius: 30,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      posts[index].username,
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      posts[index].timestamp,
-                      style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w200,
-                      ),
-                    ),
-                  ],
+      body: Column(
+        children: [
+          Row(
+            children: [
+              //Profile Picture
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 10, top: 10),
+                //Padding(padding: edgeInsets.all(10),),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    "https://picsum.photos/250?image=9",
+                  ),
+                  radius: 30,
                 ),
-                SizedBox(height: 5),
-                Column(children: [
-                  
+              ),
+              SizedBox(width: 10),
+              //Username 
+              Text(
+                "Esther Wanjiru",
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(width: 10),
+              //Timestamp
+              Text(
+                "1h ago",
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.w200),
+              ),
             ],
+          ),
+          SizedBox(height: 5),
+          //Post Content
+          Container(
+            height: 400,
+            color: Colors.grey[300],
+          ),
+          //Below the post => Post caption, like and dislike buttons
+          Row(children: [
+            Padding(padding: EdgeInsets.only(top:8, left:5),
+            child: RichText(
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w200,
+                      color: Colors.black,
+                    ),
+                  )
+                ]
+              ),
             ),
-              ],
             ),
-          );
-        },
+            Icon(Icons.arrow_upward),
+            Icon(Icons.arrow_downward),
+          ],)
+        ],
       ),
     );
   }
