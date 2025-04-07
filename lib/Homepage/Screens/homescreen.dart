bfd0ce74d6710c4b6f6ffaa892f0eacbcfeefe86
Pak_Man926 <davidkamau926@gmail.com>
2025-04-07 13:13:58 +0300

    Homepage 6.0

--- lib/Homepage/Screens/homescreen.dart
+++ lib/Homepage/Screens/homescreen.dart
@@ -1,43 +1,67 @@
 import 'package:flutter/material.dart';
+import "package:lg_tribe/Homepage/Screens/models/homescreen_model.dart";
 
 class Homescreen extends StatelessWidget {
+  List<Post> posts = [
+    Post(
+      username: "Esther Wanjiru",
+      userImageUrl: "https://picsum.photos/250?image=9",
+      timestamp: "1hr ago",
+      contentText:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      contentImageUrl: "https://picsum.photos/id/27/367/267",
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(left: 10, top: 10),
-                //Padding(padding: edgeInsets.all(10),),
-                child:CircleAvatar(
-                  backgroundImage: NetworkImage(
-                    "https://picsum.photos/250?image=9",
-                  ),
-                  radius: 30,
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (Context, index) {
+          return Card(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.only(left: 10, top: 10),
+                      //Padding(padding: edgeInsets.all(10),),
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          posts[index].userImageUrl,
+                        ),
+                        radius: 30,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      posts[index].username,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      posts[index].timestamp,
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w200,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(width: 10),
-                Text(
-                  "Esther Wanjiru",
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(width: 10),
-                Text(
-                  "1h ago",
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w200),
-                ),
+                SizedBox(height: 5),
+                Column(children: [
+                  
             ],
-          ),
-          SizedBox(height: 5),
-          Column(
-            children: [
-              
-            ],
-            )
-        ],
+            ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
---
+++ lib/Homepage/Screens/models/homescreen_model.dart
@@ -0,0 +1,15 @@
+class Post {
+  final String username;
+  final String userImageUrl;
+  final String timestamp;
+  final String contentText;
+  final String contentImageUrl;
+
+  Post({
+    required this.username,
+    required this.userImageUrl,
+    required this.timestamp,
+    required this.contentText,
+    required this.contentImageUrl,
+  });
+}
