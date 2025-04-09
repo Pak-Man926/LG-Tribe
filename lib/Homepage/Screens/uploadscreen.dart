e3ad5f860b38aa519b4a784958a97d0cb1398e17
Pak_Man926 <davidkamau926@gmail.com>
2025-04-09 13:36:51 +0300

    Upload screen 1.0

--- lib/Homepage/Screens/uploadscreen.dart
+++ lib/Homepage/Screens/uploadscreen.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
-class UploadScreen extends StatelessWidget {
+class UploadScreen extends StatefulWidget {
   const UploadScreen({super.key});
 
   @override
+  State<UploadScreen> createState() => _UploadScreenState();
+}
+
+class _UploadScreenState extends State<UploadScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Upload"),)
     );
   }
+
+  Future<void>
 }
