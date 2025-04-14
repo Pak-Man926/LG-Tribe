5c2eedfb96e8931c2743b35037d3d57ab47ea3be
Pak_Man926 <davidkamau926@gmail.com>
2025-04-14 12:32:22 +0300

    Profile Screen 1.0

---
+++ lib/Profile Page/profile.dart
@@ -0,0 +1,14 @@
+import "package:flutter/material.dart";
+import "package:lg_tribe/Homepage/Screens/homescreen.dart";
+
+class Profile extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+    body: 
+    );
+  }
+  
+}
