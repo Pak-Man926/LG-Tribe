daca9acdb13bc1ea7ccd81462bcbcd8e7ab54746
Pak_Man926 <davidkamau926@gmail.com>
2025-04-14 11:10:09 +0300

    Notification Screen

--- lib/Homepage/Screens/notificationsscreen.dart
+++ lib/Homepage/Screens/notificationsscreen.dart
@@ -6,7 +6,121 @@ class NotificationsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Notifications"),)
+      body: ListView(
+        children: [
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            padding:EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: ListTile(
+              leading: const Icon(Icons.notifications),
+              title: const Text('Lorem Ipsum'),
+              subtitle: const Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+              ),
+              trailing: const Icon(Icons.more_vert_rounded),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
