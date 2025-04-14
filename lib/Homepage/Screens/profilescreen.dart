2fde95df63ff3d34ec87fae183519bf244d688ae
Pak_Man926 <davidkamau926@gmail.com>
2025-04-14 12:12:40 +0300

    Profile Screen

--- lib/Homepage/Screens/profilescreen.dart
+++ lib/Homepage/Screens/profilescreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "package:country_flags/country_flags.dart";
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,7 +7,85 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Profile"),)
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  //Profile Picture
+                  CircleAvatar(
+                    radius: 40,
+                    // backgroundImage: NetworkImage(
+                    //   'https://example.com/profile.jpg',
+                    // ),
+                  ),
+                  // DecoratedBox(
+                  //   decoration: BoxDecoration(
+                  //     color: Colors.green,
+                  //     shape: BoxShape.circle,
+                  //     border: Border.all(color: Colors.white, width: 3),
+                  //   ),
+                  //   child: SizedBox(width: 20, height: 20),
+                  // ),
+                ],
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //UserName
+                  const Text(
+                    'Esther Wanjiru',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: [
+                      //Country Fag
+                      CountryFlag.fromCountryCode(
+                        "KE",
+                        height: 20, // Set height
+                        width:
+                            30, // Set width if you want a rectangular flag, optional
+                      ),
+                      const SizedBox(width: 5),
+                      //Country Name
+                      const Text(
+                        'Kenya, East Africa',
+                        style: TextStyle(fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          //Posts
+          Expanded(
+            child: GridView.count(
+              crossAxisCount: 3,
+              mainAxisSpacing: 5,
+              crossAxisSpacing: 5,
+              padding: const EdgeInsets.all(8),
+              children: List.generate(12, (index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://picsum.photos/200?image=${index + 24}',
+                      ),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
