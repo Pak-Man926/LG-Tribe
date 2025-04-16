841d4386c7b245af2de8a9a98927fabf8a5cac5f
Pak_Man926 <davidkamau926@gmail.com>
2025-04-16 13:27:16 +0300

    Blank

--- lib/Homepage/Profile Page/Screens/about.dart
+++ lib/Homepage/Profile Page/Screens/about.dart
@@ -25,9 +25,9 @@ class About extends StatelessWidget
           )
         )
       ),
-      body: Center(
-        child: 
-      )
+      // body: Center(
+      //   child: 
+      // )
     );
   }
 }
