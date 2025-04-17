f43090327d78d87b3fd619735e95fae57e7b6414
Pak_Man926 <davidkamau926@gmail.com>
2025-04-17 14:08:06 +0300

    About Page 1.2

--- lib/Homepage/Profile Page/Screens/about.dart
+++ lib/Homepage/Profile Page/Screens/about.dart
@@ -23,16 +23,61 @@ class About extends StatelessWidget
           "About App",
           style: TextStyle(
             fontSize: 15,
-            color: Colors.blueGrey,
+            color: const Color.fromARGB(255, 8, 8, 8),
           )
         )
       ),
        body: Center(
-         child: Column(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Hello"),
+            Image.asset(
+              "assets/LGE_Logo_HeritageRed_Grey_RGB.png",
+              height: 100,
+              width: 100,
+            ),
+            Text(
+              "LG Management System",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              )
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Version 1.0.0",
+              style: TextStyle(
+                fontSize: 15,
+                color: const Color.fromARGB(255, 199, 198, 198),
+              )
+            ),
+            SizedBox(height: 5),
+            Text(
+              "Build Number: 67",
+              style: TextStyle(
+                fontSize: 15,
+                color: const Color.fromARGB(255, 199, 198, 198),
+              )
+            ),
+            SizedBox(height: 10),
+            Text(
+              "Copyright",
+              style: TextStyle(
+                fontSize: 15,
+                color: const Color.fromARGB(255, 199, 198, 198),
+              )
+              ),
+            SizedBox(height: 3),
+            Text(
+              "© 2023 LG Tribe",
+              style: TextStyle(
+                fontSize: 15,
+                color: const Color.fromARGB(255, 199, 198, 198),
+              )
+            ),
           ],
-         ) 
+        ), 
        )
     );
   }
