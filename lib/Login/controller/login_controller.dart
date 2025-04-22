684f2b65957b5c6147656ef6e40a30c0a3ea3362
Pak_Man926 <davidkamau926@gmail.com>
2025-04-22 15:58:10 +0300

    Login Page 3.0

---
+++ lib/Login/controller/login_controller.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../../Homepage/homepage.dart';
+
+class LoginController {
+  Future<void> loginUser({
+    String? phoneNumber,
+    String? password,
+    BuildContext? context,
+  }) async {
+    final url = Uri.parse('http://localhost:8080/users');
+
+    print('✅ Url: ${url}');
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        "userName": "Alex",
+        "password": "12345678",
+        "email": "alex20@gmail.com",
+        "phoneNumber": "0721709973",
+      }),
+    );
+
+    // if (response.statusCode == 200) {
+    //   print('✅ Login successful: ${response.body}');r
+    //   Navigator.push(
+    //     context!,
+    //     MaterialPageRoute(builder: (context) => Homepage()),
+    //   );
+    // } else {
+    //   print('❌ Login failed: ${response.body}');
+    //   ScaffoldMessenger.of(context!).showSnackBar(
+    //     SnackBar(content: Text('Login failed. Check credentials.')),
+    //   );
+    // }
+  }
+}
