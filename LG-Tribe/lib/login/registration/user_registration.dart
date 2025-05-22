1b6b592da2581833f9c0fb1087f6d937aea41180
Pak_Man926 <davidkamau926@gmail.com>
2025-05-22 13:50:54 +0300

    Registration page asset alignment

--- LG-Tribe/lib/login/registration/user_registration.dart
+++ LG-Tribe/lib/login/registration/user_registration.dart
@@ -2,40 +2,49 @@ import 'package:flutter/material.dart';
 //import 'package:http/http.dart' as http;
 //import 'dart:convert';
 
-class UserRegistration extends StatelessWidget
-{
+class UserRegistration extends StatelessWidget {
   const UserRegistration({super.key});
 
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:SafeArea(
-        child: Form(
-          child: Column(
-           // mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Positioned(
-                top: 50,
-                left: 20,
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/LGE_Logo_HeritageRed_Grey_RGB.png'),
-                      fit: BoxFit.scaleDown,
+      body: Column(
+        children: [
+          SafeArea(
+            child: Positioned(
+              top: 50,
+              left: 30,
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/LGE_Logo_HeritageRed_Grey_RGB.png',
                     ),
+                    fit: BoxFit.scaleDown,
                   ),
-                  width: 100,
-                  height: 100,
                 ),
+                width: 100,
+                height: 100,
               ),
-              SizedBox(height: 50),
-              Text("Welcome to LG Tribe!", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100)),
-            ]
+            ),
+          ),
+          SizedBox(height: 30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Welcome to LG Tribe! Please enter your details to continue.",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Column(
+           crossAxisAl            
           )
-        ),
-      )
+        ],
+      ),
     );
   }
 }
