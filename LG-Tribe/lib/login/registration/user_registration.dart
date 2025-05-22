1c0d623f155c53fd128afdc9cf96717a9cd91079
Pak_Man926 <davidkamau926@gmail.com>
2025-05-22 13:15:37 +0300

    Dependacy updates

--- LG-Tribe/lib/login/registration/user_registration.dart
+++ LG-Tribe/lib/login/registration/user_registration.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 
 class UserRegistration extends StatelessWidget
 {
+  const UserRegistration({super.key});
+
   @override
   Widget build(BuildContext context)
   {
