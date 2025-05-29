9638e8cc682b4d065804c33376bd6225ece495ae
Pak_Man926 <davidkamau926@gmail.com>
2025-05-29 15:41:09 +0300

    Auto commit

--- LG-Tribe/lib/Start_Page/startpage.dart
+++ LG-Tribe/lib/Start_Page/startpage.dart
@@ -1,5 +1,5 @@
 import "package:flutter/material.dart";
-import "package:lg_tribe/Country/country.dart";
+//import "package:lg_tribe/Country/country.dart";
 import "package:get/get.dart";
 
 class Startpage extends StatelessWidget {
