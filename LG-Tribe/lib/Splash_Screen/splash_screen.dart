0fe4ad4b03e284585f24389fd37a3cc1cd579e41
Pak_Man926 <davidkamau926@gmail.com>
2025-05-30 15:26:53 +0300

    Auto commit

--- LG-Tribe/lib/Splash_Screen/splash_screen.dart
+++ LG-Tribe/lib/Splash_Screen/splash_screen.dart
@@ -25,6 +25,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // Getoff(NextScreen());
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
