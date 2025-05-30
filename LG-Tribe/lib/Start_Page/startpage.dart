19d78cdf03d1ccc2a56814591fd763e22d920de2
Pak_Man926 <davidkamau926@gmail.com>
2025-05-30 13:19:31 +0300

    Auto commit

--- LG-Tribe/lib/Start_Page/startpage.dart
+++ LG-Tribe/lib/Start_Page/startpage.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 //import "package:lg_tribe/Country/country.dart";
 import "package:get/get.dart";
 
-//Startpage. User chooses
+//Startpage. User chooses a role to proceed with the app.
 class Startpage extends StatelessWidget {
   const Startpage({super.key});
 
