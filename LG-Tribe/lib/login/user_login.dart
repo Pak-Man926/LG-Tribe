946aa34e7bb707e3c4693d4cc3d34ad66a84d6d4
Pak_Man926 <davidkamau926@gmail.com>
2025-06-06 17:33:03 +0300

    Auto commit

--- LG-Tribe/lib/login/user_login.dart
+++ LG-Tribe/lib/login/user_login.dart
@@ -10,7 +10,7 @@ import "package:lg_tribe/Controller/controller_class.dart";
 
 class Login extends StatelessWidget 
 {
-  final userState = Get.put
+  final userState = Get.put(UserStateController());
 
   @override
   Widget build(BuildContext context) {
