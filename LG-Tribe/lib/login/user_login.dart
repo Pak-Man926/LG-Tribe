db2dba22e8acedb77c7f3d3a341209a64adb956b
Pak_Man926 <davidkamau926@gmail.com>
2025-06-09 23:16:38 +0300

    Auto commit

--- LG-Tribe/lib/login/user_login.dart
+++ LG-Tribe/lib/login/user_login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
+import "package:intl_phone_field/phone_number.dart";
 //import 'package:lg_tribe/Forgot%20Password/forgot_password.dart';
 //import 'package:lg_tribe/Homepage/homepage.dart';
 //import "package:lg_tribe/login/registration/user_registration.dart";
@@ -140,11 +141,11 @@ class Login extends StatelessWidget
                             border: OutlineInputBorder(),
                           ),
                           initialCountryCode: initialCountryCode,
-                          onChanged: (phone) {
+                          onChanged: (PhoneNumber) {
                             // setState(() {
                             //   phoneNumber = phone.completeNumber;
                             // });
-                            phone = phone.completeNumber;
+                            PhoneNumber.number = PhoneNumber.completeNumber;
                           },
                         ),
                         SizedBox(height: 5),
