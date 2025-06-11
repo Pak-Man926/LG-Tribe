a6386662e9fa96e2a657b168963adcbce375ad12
Pak_Man926 <davidkamau926@gmail.com>
2025-06-11 15:52:48 +0300

    Auto commit

--- LG-Tribe-Server/lib/server.dart
+++ LG-Tribe-Server/lib/server.dart
@@ -1,7 +1,7 @@
-import 'package:lg_tribe_server/src/birthday_reminder.dart';
+//import 'package:lg_tribe_server/src/birthday_reminder.dart';
 import 'package:serverpod/serverpod.dart';
 
-import 'package:lg_tribe_server/src/web/routes/root.dart';
+//import 'package:lg_tribe_server/src/web/routes/root.dart';
 
 import 'src/generated/protocol.dart';
 import 'src/generated/endpoints.dart';
@@ -19,13 +19,13 @@ void run(List<String> args) async {
   );
 
   // Setup a default page at the web root.
-  pod.webServer.addRoute(RouteRoot(), '/');
-  pod.webServer.addRoute(RouteRoot(), '/index.html');
-  // Serve all files in the /static directory.
-  pod.webServer.addRoute(
-    RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
-    '/*',
-  );
+  // pod.webServer.addRoute(RouteRoot(), '/');
+  // pod.webServer.addRoute(RouteRoot(), '/index.html');
+  // // Serve all files in the /static directory.
+  // pod.webServer.addRoute(
+  //   RouteStaticDirectory(serverDirectory: 'static', basePath: '/'),
+  //   '/*',
+  // );
 
   // Start the server.
   await pod.start();
@@ -37,30 +37,30 @@ void run(List<String> args) async {
   // background. Their schedule is persisted to the database, so you will not
   // lose them if the server is restarted.
 
-  pod.registerFutureCall(
-    BirthdayReminder(),
-    FutureCallNames.birthdayReminder.name,
-  );
+  // pod.registerFutureCall(
+  //   BirthdayReminder(),
+  //   FutureCallNames.birthdayReminder.name,
+  // );
 
   // You can schedule future calls for a later time during startup. But you can also
   // schedule them in any endpoint or webroute through the session object.
   // there is also [futureCallAtTime] if you want to schedule a future call at a
   // specific time.
-  await pod.futureCallWithDelay(
-    FutureCallNames.birthdayReminder.name,
-    Greeting(
-      message: 'Hello!',
-      author: 'Serverpod Server',
-      timestamp: DateTime.now(),
-    ),
-    Duration(seconds: 5),
-  );
+  // await pod.futureCallWithDelay(
+  //   FutureCallNames.birthdayReminder.name,
+  //   Greeting(
+  //     message: 'Hello!',
+  //     author: 'Serverpod Server',
+  //     timestamp: DateTime.now(),
+  //   ),
+  //   Duration(seconds: 5),
+  // );
 }
 
 /// Names of all future calls in the server.
 ///
 /// This is better than using a string literal, as it will reduce the risk of
 /// typos and make it easier to refactor the code.
-enum FutureCallNames {
-  birthdayReminder,
-}
+// enum FutureCallNames {
+//   birthdayReminder,
+// }
