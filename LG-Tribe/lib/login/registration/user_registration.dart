02cf5c30384a4dab01f7594b4efb7fc828cef0c9
Pak_Man926 <davidkamau926@gmail.com>
2025-06-13 03:20:29 +0300

    Auto commit

--- LG-Tribe/lib/login/registration/user_registration.dart
+++ LG-Tribe/lib/login/registration/user_registration.dart
@@ -103,237 +103,237 @@ class UserRegistration extends StatelessWidget {
     );
 
     return Scaffold(
-      body: Column(
-        children: [
-          SafeArea(
-            child: Positioned(
-              top: 50,
-              left: 30,
-              child: Container(
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage(
-                      'assets/LGE_Logo_HeritageRed_Grey_RGB.png',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Positioned(
+                top: 50,
+                left: 30,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/LGE_Logo_HeritageRed_Grey_RGB.png',
+                      ),
+                      fit: BoxFit.scaleDown,
                     ),
-                    fit: BoxFit.scaleDown,
                   ),
+                  width: 100,
+                  height: 100,
                 ),
-                width: 100,
-                height: 100,
               ),
             ),
-          ),
-          SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Welcome to LG Tribe! Please enter your details to continue.",
-                style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
-              ),
-            ],
-          ),
-          SizedBox(height: 10),
-          SingleChildScrollView(
-            child: Form(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "First Name:",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Welcome to LG Tribe! Please enter your details to continue.",
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+               Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "First Name:",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: firstNameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your first name',
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: firstNameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your first name',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Last Name:",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "Last Name:",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: lastNameController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your last name',
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: lastNameController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your last name',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Email:",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "Email:",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: emailController,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your email address',
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your email address',
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Phone Number:",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "Phone Number:",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: IntlPhoneField(
-                      controller: contactNumberController,
-                      decoration: const InputDecoration(
-                        hintText: "Enter your phone number",
-                        border: OutlineInputBorder(),
-                      ),
-                      initialCountryCode: initialCountryCode,
-                      onChanged: (PhoneNumber) {
-                        // setState(() {
-                        //   phoneNumber = phone.completeNumber;
-                        // });
-                        PhoneNumber.number = PhoneNumber.completeNumber;
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  //Enter country of origin
-                  DropdownButton(
-                    items: const [
-                      DropdownMenuItem(
-                        value: CountryLocated.kenya,
-                        child: Text("Kenya"),
-                      ),
-                      DropdownMenuItem(
-                        value: CountryLocated.ethopia,
-                        child: Text("Ethiopia"),
-                      ),
-                      DropdownMenuItem(
-                        value: CountryLocated.tanzania,
-                        child: Text("Tanzania"),
-                      ),
-                      DropdownMenuItem(
-                        value: CountryLocated.sudan,
-                        child: Text("Sudan"),
-                      ),
-                      DropdownMenuItem(
-                        value: CountryLocated.uganda,
-                        child: Text("Uganda"),
-                      ),
-                      DropdownMenuItem(
-                        value: CountryLocated.zambia,
-                        child: Text("Zambia"),
-                      ),
-                    ],
-                    onChanged: (CountryLocated? selectedCountry) {
-                      userState.setCountry(selectedCountry);
-                    },
-                    iconEnabledColor: Color(0xFFA50034),
-                    isExpanded: true,
-                  ),
-                  SizedBox(height: 10),
-                  //Enter user auth level
-                  DropdownButton(
-                    items: const [
-                      DropdownMenuItem(
-                        value: AuthLevel.merchandiser,
-                        child: Text("Merchandiser"),
-                      ),
-                      DropdownMenuItem(
-                        value: AuthLevel.promoter,
-                        child: Text("Promoter"),
-                      ),
-                      DropdownMenuItem(
-                        value: AuthLevel.floorServiceprovider,
-                        child: Text("Floor Service Provider"),
-                      ),
-                    ],
-                    onChanged: (AuthLevel? newValue) {
-                      userState.setAuthLevel(newValue);
-                    },
-                    iconEnabledColor: Color(0xFFA50034),
-                    isExpanded: true,
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10),
-                    child: Text(
-                      "Password:",
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 5),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: TextFormField(
-                      controller: passwordController,
-                      keyboardType: TextInputType.visiblePassword,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Enter your password',
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 120),
-                    child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: FilledButton(
-                        onPressed: () async {
-                          await _register();
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: IntlPhoneField(
+                        controller: contactNumberController,
+                        decoration: const InputDecoration(
+                          hintText: "Enter your phone number",
+                          border: OutlineInputBorder(),
+                        ),
+                        initialCountryCode: initialCountryCode,
+                        onChanged: (PhoneNumber) {
+                          // setState(() {
+                          //   phoneNumber = phone.completeNumber;
+                          // });
+                          PhoneNumber.number = PhoneNumber.completeNumber;
                         },
-                        child: Text("Create Account"),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 5),
+                    //Enter country of origin
+                    DropdownButton(
+                      items: const [
+                        DropdownMenuItem(
+                          value: CountryLocated.kenya,
+                          child: Text("Kenya"),
+                        ),
+                        DropdownMenuItem(
+                          value: CountryLocated.ethopia,
+                          child: Text("Ethiopia"),
+                        ),
+                        DropdownMenuItem(
+                          value: CountryLocated.tanzania,
+                          child: Text("Tanzania"),
+                        ),
+                        DropdownMenuItem(
+                          value: CountryLocated.sudan,
+                          child: Text("Sudan"),
+                        ),
+                        DropdownMenuItem(
+                          value: CountryLocated.uganda,
+                          child: Text("Uganda"),
+                        ),
+                        DropdownMenuItem(
+                          value: CountryLocated.zambia,
+                          child: Text("Zambia"),
+                        ),
+                      ],
+                      onChanged: (CountryLocated? selectedCountry) {
+                        userState.setCountry(selectedCountry);
+                      },
+                      iconEnabledColor: Color(0xFFA50034),
+                      isExpanded: true,
+                    ),
+                    SizedBox(height: 10),
+                    //Enter user auth level
+                    DropdownButton(
+                      items: const [
+                        DropdownMenuItem(
+                          value: AuthLevel.merchandiser,
+                          child: Text("Merchandiser"),
+                        ),
+                        DropdownMenuItem(
+                          value: AuthLevel.promoter,
+                          child: Text("Promoter"),
+                        ),
+                        DropdownMenuItem(
+                          value: AuthLevel.floorServiceprovider,
+                          child: Text("Floor Service Provider"),
+                        ),
+                      ],
+                      onChanged: (AuthLevel? newValue) {
+                        userState.setAuthLevel(newValue);
+                      },
+                      iconEnabledColor: Color(0xFFA50034),
+                      isExpanded: true,
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Text(
+                        "Password:",
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
+                        controller: passwordController,
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter your password',
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 120),
+                      child: Align(
+                        alignment: Alignment.bottomCenter,
+                        child: FilledButton(
+                          onPressed: () async {
+                            await _register();
+                          },
+                          child: Text("Create Account"),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
