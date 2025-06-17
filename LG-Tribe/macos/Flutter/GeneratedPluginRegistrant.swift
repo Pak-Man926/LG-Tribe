f364cea0d0def5116f4912f2980e244b7e110d04
Pak_Man926 <davidkamau926@gmail.com>
2025-06-17 22:41:49 +0300

    Generated files

--- LG-Tribe/macos/Flutter/GeneratedPluginRegistrant.swift
+++ LG-Tribe/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import connectivity_plus
 import gal
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
