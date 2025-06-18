9660cb8163b6b70701715dcdec51b154b8b77c7a
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 16:58:52 +0300

    Auto commit

--- LG-Tribe/lib/Controller/userstate_controller_class.dart
+++ LG-Tribe/lib/Controller/userstate_controller_class.dart
@@ -2,54 +2,47 @@ import "package:get/get.dart";
 import "package:lg_tribe/Controller/classes/authlevel_class.dart";
 import "package:lg_tribe/Controller/classes/country_located_class.dart";
 
-class UserStateController extends GetxController
-{
+class UserStateController extends GetxController {
   var selectedAuthLevel = AuthLevel.merchandiser.obs;
   var selectedCountry = CountryLocated.kenya.obs;
   var isLoggedIn = false.obs;
-  
+
   //Auth level getter
-  AuthLevel getAuthLevel()
-  {
+  AuthLevel getAuthLevel() {
     return selectedAuthLevel.value;
   }
+
   //Auth level setter
-  void setAuthLevel(AuthLevel value)
-  {
+  void setAuthLevel(AuthLevel value) {
     selectedAuthLevel.value = value;
   }
-  
+
   //Country getter
-  CountryLocated getCountry()
-  {
+  CountryLocated getCountry() {
     return selectedCountry.value;
   }
+
   //Country setter
-  void setCountry(CountryLocated country)
-  {
+  void setCountry(CountryLocated country) {
     selectedCountry.value = country;
   }
 
-    String getInitialCountryCode(CountryLocated country)
-{
-  switch (country) {
-    case CountryLocated.kenya:
-      return 'KE';
-    case CountryLocated.ethopia:
-      return 'ET';
-    case CountryLocated.tanzania:
-      return 'TZ';
-    case CountryLocated.sudan:
-      return 'SD';  
-    case CountryLocated.uganda:
-      return 'UG';
-    case CountryLocated.zambia:
-      return 'ZM';
-    // default:
-    //   return 'KE'; // Default to Kenya if no country is selected
+  String getInitialCountryCode(CountryLocated country) {
+    switch (country) {
+      case CountryLocated.kenya:
+        return 'KE';
+      case CountryLocated.ethopia:
+        return 'ET';
+      case CountryLocated.tanzania:
+        return 'TZ';
+      case CountryLocated.sudan:
+        return 'SD';
+      case CountryLocated.uganda:
+        return 'UG';
+      case CountryLocated.zambia:
+        return 'ZM';
+      // default:
+      //   return 'KE'; // Default to Kenya if no country is selected
+    }
   }
 }
-  
-  
-}   
-    
