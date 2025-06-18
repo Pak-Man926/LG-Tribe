58cc80bb21c321168db36c35bb0be94473dcc3cc
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 17:09:37 +0300

    Auto commit

--- LG-Tribe/lib/Controller/userstate_controller_class.dart
+++ LG-Tribe/lib/Controller/userstate_controller_class.dart
@@ -27,22 +27,5 @@ class UserStateController extends GetxController {
     selectedCountry.value = country;
   }
 
-  String getInitialCountryCode(CountryLocated country) {
-    switch (country) {
-      case CountryLocated.kenya:
-        return 'KE';
-      case CountryLocated.ethopia:
-        return 'ET';
-      case CountryLocated.tanzania:
-        return 'TZ';
-      case CountryLocated.sudan:
-        return 'SD';
-      case CountryLocated.uganda:
-        return 'UG';
-      case CountryLocated.zambia:
-        return 'ZM';
-      default:
-        return 'KE'; // Default to Kenya if no country is selected
-    }
-  }
+  
 }
