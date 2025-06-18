681bfdda2072cbcefc34e6f2ae108f76aa5221f4
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 04:40:40 +0300

    Auto commit

---
+++ LG-Tribe/lib/Controller/classes/authlevel_class.dart
@@ -0,0 +1,22 @@
+import 'dart:math';
+
+class AuthLevel 
+{
+  final String value;
+
+  const AuthLevel._(this.value);
+
+  static const merchandiser = AuthLevel._("Merchandiser");
+  static const promoter = AuthLevel._("Promoter");
+  static const floorServiceprovider = AuthLevel._("Floor Service Provider");
+
+  static const values = [
+    merchandiser,
+    promoter,
+    floorServiceprovider
+  ];
+
+  static AuthLevel fromString(String val) =>
+    values.firstWhere((e) => e.value == val, orElse: () => throw ArgumentError("Invalid AuthLevel value: $val"));
+  
+}
