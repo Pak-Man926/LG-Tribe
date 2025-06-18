b61a6a2330444954052d68c0feeeb7447734588d
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 14:15:10 +0300

    Auto commit

--- LG-Tribe/lib/Start_Page/startpage.dart
+++ LG-Tribe/lib/Start_Page/startpage.dart
@@ -9,97 +9,89 @@ class Startpage extends StatelessWidget {
 
   final userState = Get.put(UserStateController());
 
-  UserStateController controller = Get.find();
-
-  //Get.offAll(Nextpage());
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(body: StartPage(context)),
-    );
-  }
-
-  Column StartPage(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 100, // Diameter of CircleAvatar (2 * radius)
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/LGE_Logo_HeritageRed_Grey_RGB.png'),
-                  fit:
-                      BoxFit
-                          .scaleDown, // Ensures the image covers the circle fully
+    Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 100, // Diameter of CircleAvatar (2 * radius)
+                height: 100,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage('assets/LGE_Logo_HeritageRed_Grey_RGB.png'),
+                    fit:
+                        BoxFit
+                            .scaleDown, // Ensures the image covers the circle fully
+                  ),
                 ),
               ),
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  "LG Management System",
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Padding(padding: EdgeInsets.all(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    "LG Management System",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(padding: EdgeInsets.all(10)),
+                  Text(
+                    "Proceed as:",
+                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.all(10)),
+              SizedBox(
+                width: 250,
+                child: OutlinedButton(
+                  //Merchandiser route
+                  onPressed: () {
+                    userState.setAuthLevel(AuthLevel.merchandiser);
+                    Get.toNamed("/country");
+                  },
+                  child: Text("Merchandiser"),
                 ),
-                Padding(padding: EdgeInsets.all(10)),
-                Text(
-                  "Proceed as:",
-                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.w100),
+              ),
+              Padding(padding: EdgeInsets.all(10)),
+              SizedBox(
+                width: 250,
+                child: OutlinedButton(
+                  //Promoter route
+                  onPressed: () {
+                    userState.setAuthLevel(AuthLevel.promoter);
+                    Get.toNamed("/country");
+                  },
+                  child: Text("Promoter"),
                 ),
-              ],
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            Padding(padding: EdgeInsets.all(10)),
-            SizedBox(
-              width: 250,
-              child: OutlinedButton(
-                //Merchandiser route
-                onPressed: () {
-                  userState.setAuthLevel(AuthLevel.merchandiser);
-                  Get.toNamed("/country");
-                },
-                child: Text("Merchandiser"),
               ),
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            SizedBox(
-              width: 250,
-              child: OutlinedButton(
-                //Promoter route
-                onPressed: () {
-                  userState.setAuthLevel(AuthLevel.promoter);
-                  Get.toNamed("/country");
-                },
-                child: Text("Promoter"),
+              Padding(padding: EdgeInsets.all(10)),
+              SizedBox(
+                width: 250,
+                child: OutlinedButton(
+                  //Floor Service Provider route
+                  onPressed: () {
+                    userState.setAuthLevel(AuthLevel.floorServiceprovider);
+                    Get.toNamed("/country");
+                  },
+                  child: Text("Floor Service Provider"),
+                ),
               ),
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-            SizedBox(
-              width: 250,
-              child: OutlinedButton(
-                //Floor Service Provider route
-                onPressed: () {
-                  userState.setAuthLevel(AuthLevel.floorServiceprovider);
-                  Get.toNamed("/country");
-                },
-                child: Text("Floor Service Provider"),
-              ),
-            ),
-            Padding(padding: EdgeInsets.all(10)),
-          ],
-        ),
-      ],
+              Padding(padding: EdgeInsets.all(10)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
