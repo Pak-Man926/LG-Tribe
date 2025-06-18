9bede2a121503b9c4f02496815241e93e0634c15
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 16:23:45 +0300

    Auto commit

--- LG-Tribe/lib/Start_Page/startpage.dart
+++ LG-Tribe/lib/Start_Page/startpage.dart
@@ -57,7 +57,7 @@ class Startpage extends StatelessWidget {
                 child: OutlinedButton(
                   //Merchandiser route
                   onPressed: () {
-                    userState.setAuthLevel(AuthLevel.merchandiser);
+                    userState.getAuthLevel(AuthLevel.merchandiser);
                     Get.toNamed("/country");
                   },
                   child: Text("Merchandiser"),
