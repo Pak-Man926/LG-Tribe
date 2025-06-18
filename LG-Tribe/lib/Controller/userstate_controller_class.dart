05a9e7a08b054c98f520ef8e14efe59e524e1b51
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 04:48:06 +0300

    Auto commit

--- LG-Tribe/lib/Controller/userstate_controller_class.dart
+++ LG-Tribe/lib/Controller/userstate_controller_class.dart
@@ -2,27 +2,8 @@ import "package:get/get.dart";
 
 class UserStateController extends GetxController
 {
-  var selectedAuthLevel = Rx<AuthLevel?>(null);
-  var selectedCountry = Rx<CountryLocated?>(null);
+  var selectedAuthLevel = 
+  var selectedCountry = 
   var isLoggedIn = false.obs;
   
-
-  void setAuthLevel(AuthLevel? level)
-  {
-    selectedAuthLevel.value = level;  
-  }
-
-  void setCountry(CountryLocated? country)
-  {
-    selectedCountry.value = country;  
-  }
-
-  AuthLevel? getAuthLevel()
-  {
-    return selectedAuthLevel.value;
-  }
-  CountryLocated? getCountry()
-  {
-    return selectedCountry.value;
-  }
 }  
