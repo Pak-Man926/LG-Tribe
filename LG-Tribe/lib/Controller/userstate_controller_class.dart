085b12f4989054f7dc0009b4ec9a8265c139bae5
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 04:51:22 +0300

    Auto commit

--- LG-Tribe/lib/Controller/userstate_controller_class.dart
+++ LG-Tribe/lib/Controller/userstate_controller_class.dart
@@ -1,9 +1,11 @@
 import "package:get/get.dart";
+import 'authlevel_class.dart';
+import 'countrylocated_class.dart';
 
 class UserStateController extends GetxController
 {
-  var selectedAuthLevel = 
-  var selectedCountry = 
+  var selectedAuthLevel = AuthLevel.merchandiser.obs;
+  var selectedCountry = CountryLocated.kenya.obs;
   var isLoggedIn = false.obs;
   
 }  
