311594f3d647289a4ed6991f6941943dd75e9762
Pak_Man926 <davidkamau926@gmail.com>
2025-06-18 14:18:25 +0300

    Auto commit

--- LG-Tribe/lib/main.dart
+++ LG-Tribe/lib/main.dart
@@ -49,16 +49,5 @@ void main() async
   );
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: SplashScreen(),
-//     );
-//   }
-// }
 
 
