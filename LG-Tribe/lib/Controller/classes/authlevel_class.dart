676555a8697328869cd11a93c6f9a2f1a3b63bd8
Pak_Man926 <davidkamau926@gmail.com>
2025-06-19 15:10:29 +0300

    Auto commit

--- LG-Tribe/lib/Controller/classes/authlevel_class.dart
+++ LG-Tribe/lib/Controller/classes/authlevel_class.dart
@@ -1,5 +1,4 @@
-class AuthLevel 
-{
+class AuthLevel {
   final String value;
 
   const AuthLevel._(this.value);
@@ -8,27 +7,22 @@ class AuthLevel
   static const promoter = AuthLevel._("Promoter");
   static const floorServiceprovider = AuthLevel._("Floor Service Provider");
 
-  static const values = [
-    merchandiser,
-    promoter,
-    floorServiceprovider
-  ];
+  static const values = [merchandiser, promoter, floorServiceprovider];
 
   static AuthLevel fromString(String val) =>
-    values.firstWhere((e) => e.value == val, orElse: () => merchandiser);
-    
-    // () => throw ArgumentError("Invalid AuthLevel value: $val"));
-  
-  // Inside AuthLevel class
-AuthenticationLevel toServerModel() {
-  switch (this) {
-    case AuthLevel.merchandiser:
-      return AuthenticationLevel.merchandiser;
-    case AuthLevel.promoter:
-      return AuthenticationLevel.promoter;
-    case AuthLevel.floorServiceprovider:
-      return AuthenticationLevel.floorServiceprovider;
-  }
-}
+      values.firstWhere((e) => e.value == val, orElse: () => merchandiser);
 
+  // () => throw ArgumentError("Invalid AuthLevel value: $val"));
+
+  // Inside AuthLevel class
+  AuthenticationLevel toServerModel() {
+    switch (this) {
+      case AuthLevel.merchandiser:
+        return AuthenticationLevel.merchandiser;
+      case AuthLevel.promoter:
+        return AuthenticationLevel.promoter;
+      case AuthLevel.floorServiceprovider:
+        return AuthenticationLevel.floorServiceprovider;
+    }
+  }
 }
