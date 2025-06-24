e8fef415fb7ab07fc77bc13a4ab28998e15fb491
Pak_Man926 <davidkamau926@gmail.com>
2025-06-24 23:22:11 +0300

    Auto commit

--- LG-Tribe/lib/login/user_login.dart
+++ LG-Tribe/lib/login/user_login.dart
@@ -131,6 +131,10 @@ class Login extends StatelessWidget {
                         child: MaterialButton(
                           onPressed: () async {
                             //Attempt to login
+                            final phoneNumber = contactNumberController.text;
+                            final password = passwordController.text;
+
+                            
                             // await _login();
                           },
                           color: Color(0xFFA50034),
