fc89b354b0c5cea76626f2f27f65490956fc9444
Pak_Man926 <davidkamau926@gmail.com>
2025-06-26 21:37:43 +0300

    Auto commit

--- LG-Tribe-Server/lib/src/endpoints/user_endpoints.dart
+++ LG-Tribe-Server/lib/src/endpoints/user_endpoints.dart
@@ -5,10 +5,16 @@ import "package:lg_tribe_server/src/generated/auth_level.dart";
 import "package:lg_tribe_server/src/generated/country.dart";
 import "package:bcrypt/bcrypt.dart";
 
-class UserEndpoints extends Endpoint 
-{
-  Future<bool> registerUser(Session session, String firstName, String lastName, int contacts, String email, String password, AuthenticationLevel authenticationlevel, Country country) async
-  {
+class UserEndpoints extends Endpoint {
+  Future<bool> registerUser(
+      Session session,
+      String firstName,
+      String lastName,
+      int contacts,
+      String email,
+      String password,
+      AuthenticationLevel authenticationlevel,
+      Country country) async {
     var existingUser = await User.db.findFirstRow(
       session,
       where: (t) => t.contacts.equals(contacts),
@@ -22,40 +28,40 @@ class UserEndpoints extends Endpoint
     final hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
 
     final user = User(
-      firstName:firstName,
-      lastName: lastName,
-      contacts: contacts,
-      email: email,
-      password: hashedPassword, 
-      authlevel: authenticationlevel,
-      country: country
-    );
+        firstName: firstName,
+        lastName: lastName,
+        contacts: contacts,
+        email: email,
+        password: hashedPassword,
+        authlevel: authenticationlevel,
+        country: country);
 
     await User.db.insertRow(session, user);
 
     return true;
   }
 
-  Future<User?> loginUser(Session session, int contacts, String password, AuthenticationLevel authenticationlevel, Country country) async
-  {
-    if (password.isEmpty || contacts.toString().isEmpty || contacts.toString().length < 10){
+  Future<User?> loginUser(Session session, int contacts, String password,
+      AuthenticationLevel authenticationlevel, Country country) async {
+    if (password.isEmpty ||
+        contacts.toString().isEmpty) {
       return null; // Invalid credentials
     }
-    
+
     // Find the user with the given contacts and password
     var user = await User.db.findFirstRow(
       session,
-      where: (t) => t.contacts.equals(contacts) & t.password.equals(password) &
-                   t.authlevel.equals(authenticationlevel) &
-                   t.country.equals(country),
+      where: (t) =>
+          t.contacts.equals(contacts) &
+          t.password.equals(password) &
+          t.authlevel.equals(authenticationlevel) &
+          t.country.equals(country),
     );
 
-    if (user == null || !BCrypt.checkpw(password, user.password)) 
-    {
+    if (user == null || !BCrypt.checkpw(password, user.password)) {
       return null; // Invalid credentials
     }
 
     return user; // Return the found user
   }
-  
 }
