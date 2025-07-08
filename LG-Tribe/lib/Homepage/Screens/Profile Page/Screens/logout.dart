9503f7c18ad10c49e8c6be0e6e8d907b709e9f37
Pak_Man926 <davidkamau926@gmail.com>
2025-07-08 21:55:57 +0300

    Logout text configuration

--- LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
+++ LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
@@ -5,9 +5,22 @@ class Logout extends StatelessWidget
   Widget build(BuildContext context)
   {
     return Scaffold(
-      body: Card.filled(
-        mainAxisAlignment:MainAxisAlignment.end
-        child: 
+      body:Padding(
+        padding: EdgeInsetsGeometry.fromLTRB(0, 150, 0, 0),
+        child: Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text("Log Out", 
+              style: TextStyle(fontWeight: FontWeight.w700),
+              textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 5),
+              Text("Are you sure you want to log out of your account?",
+              style: TextStyle(fontWeight: FontWeight.w300))
+            ]
+          )
+        )
       )
     );
   }
