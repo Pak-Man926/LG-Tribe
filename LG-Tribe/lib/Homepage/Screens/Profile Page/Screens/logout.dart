59ad8a9ba5655f12a1ad72e752bbcac2667c3cc2
Pak_Man926 <davidkamau926@gmail.com>
2025-07-08 22:11:20 +0300

    Button design

--- LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
+++ LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
@@ -1,29 +1,57 @@
 import "package:flutter/material.dart";
 
-class Logout extends StatelessWidget
-{
-  Widget build(BuildContext context)
-  {
+class Logout extends StatelessWidget {
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:Padding(
+      body: Padding(
         padding: EdgeInsetsGeometry.fromLTRB(0, 150, 0, 0),
         child: Card(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Text("Log Out", 
-              style: TextStyle(fontWeight: FontWeight.w700),
-              textAlign: TextAlign.center,
+              Text(
+                "Log Out",
+                style: TextStyle(fontWeight: FontWeight.w700),
+                textAlign: TextAlign.center,
               ),
               SizedBox(height: 5),
-              Text("Are you sure you want to log out of your account?",
-              style: TextStyle(fontWeight: FontWeight.w300)),
+              Text(
+                "Are you sure you want to log out of your account?",
+                style: TextStyle(fontWeight: FontWeight.w300),
+              ),
               SizedBox(height: 10),
-              
-            ]
-          )
-        )
-      )
+              SizedBox(
+                width: double.infinity,
+                child: MaterialButton(
+                  onPressed: () {
+                    //Attempt to logout
+                  },
+                  color: Color(0xFFA50034),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.vertical(),
+                  ),
+                  child: Text("Yes, logout", style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              SizedBox(height: 5),
+              SizedBox(
+                        width: double.infinity,
+                        child: MaterialButton(
+                          //onPressed: () => Get.toNamed("/startpage"),
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.vertical(),
+                          ),
+                          child: Text(
+                            "No, take me back",
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
