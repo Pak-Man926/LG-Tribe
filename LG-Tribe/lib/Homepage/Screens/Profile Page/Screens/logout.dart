12a857f48f628b2532307ebf7712e3ff24faa9da
Pak_Man926 <davidkamau926@gmail.com>
2025-07-09 22:08:26 +0300

    Auto commit

--- LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
+++ LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 class Logout extends StatelessWidget {
+  final loginState = Get.find<LoginController>();
+  
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
