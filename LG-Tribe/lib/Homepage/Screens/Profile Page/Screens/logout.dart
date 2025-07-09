fdedab73b4848732f9c792f1542aa30c3e6ad736
Pak_Man926 <davidkamau926@gmail.com>
2025-07-09 22:31:10 +0300

    Auto commit

--- LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
+++ LG-Tribe/lib/Homepage/Screens/Profile Page/Screens/logout.dart
@@ -1,58 +1,60 @@
-import "package:flutter/material.dart";
-import "package:get/get.dart";
-import "package:lg_tribe/Controller/login_controller_class.dart";
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lg_tribe/Controller/login_controller_class.dart';
+
 class Logout extends StatelessWidget {
   final loginState = Get.find<LoginController>();
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsetsGeometry.fromLTRB(0, 150, 0, 0),
-        child: Card(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "Log Out",
-                style: TextStyle(fontWeight: FontWeight.w700),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 5),
-              Text(
-                "Are you sure you want to log out of your account?",
-                style: TextStyle(fontWeight: FontWeight.w300),
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                width: double.infinity,
-                child: MaterialButton(
-                  onPressed: () async{
-                    //Attempt to logout
-                    await loginState.logout();
-                  },
-                  color: Color(0xFFA50034),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(),
+      body: Align(
+        alignment: Alignment.bottomCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0), // Optional: some spacing
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Log Out",
+                    style: TextStyle(fontWeight: FontWeight.w700),
+                    textAlign: TextAlign.center,
                   ),
-                  child: Text("Yes, logout", style: TextStyle(color: Colors.white)),
-                ),
-              ),
-              SizedBox(height: 5),
-              SizedBox(
-                width: double.infinity,
-                child: MaterialButton(
-                  onPressed: () async{
-                    //Cancel logout
-                    Get.toNamed("/profilepage");
-                  },
-                  color: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.vertical(),
+                  SizedBox(height: 5),
+                  Text(
+                    "Are you sure you want to log out of your account?",
+                    style: TextStyle(fontWeight: FontWeight.w300),
                   ),
-                  child: Text("No, take me back", style: TextStyle(color: Colors.black)),
-                ),
+                  SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        await loginState.logout();
+                      },
+                      color: Color(0xFFA50034),
+                      shape: RoundedRectangleBorder(),
+                      child: Text("Yes, logout", style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  SizedBox(
+                    width: double.infinity,
+                    child: MaterialButton(
+                      onPressed: () async {
+                        Get.toNamed("/profilepage");
+                      },
+                      color: Colors.white,
+                      shape: RoundedRectangleBorder(),
+                      child: Text("No, take me back", style: TextStyle(color: Colors.black)),
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
