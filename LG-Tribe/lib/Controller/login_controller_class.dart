539f8a703e1c74d89f75261068fbfc48f1c17747
Pak_Man926 <davidkamau926@gmail.com>
2025-07-10 23:32:42 +0300

    Auto commit

--- LG-Tribe/lib/Controller/login_controller_class.dart
+++ LG-Tribe/lib/Controller/login_controller_class.dart
@@ -41,6 +41,61 @@ class LoginController extends GetxController {
       Get.offAllNamed("/homepage");
       return;
     }
+    else{
+      if (password.isEmpty || contactNumber.toString().isEmpty) 
+    {
+      // Show error message if any field is empty
+      Get.snackbar(
+        "Error",
+        "Please fill in all the fields!",
+        snackPosition: SnackPosition.BOTTOM,
+      );
+      return;
+    }
+
+    try {
+      final result = await client.userEndpoints.loginUser(
+        contactNumber,
+        password,
+        authenticationLevel,
+        country,
+      );
+
+      if (result == true) {
+        //Login Successful
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setInt('contacts', contactNumber);
+        await prefs.setString('password', password);
+        await prefs.setString('authLevel', authenticationLevel.name);
+        await prefs.setString('country', country.name);
+
+        Get.snackbar(
+          "Login Successful",
+           "Welcome back!",
+          snackPosition: SnackPosition.BOTTOM,
+        );
+
+        //Redirect to the homepage
+        Get.offAllNamed("/homepage");
+      } else {
+        // Login failed
+
+        Get.snackbar(
+          "Error",
+          "Invalid Credentials",
+          snackPosition: SnackPosition.BOTTOM,
+        );
+      }
+    } catch (e) {
+      print('Error during login: $e');
+
+      Get.snackbar(
+        "Error",
+        "An error occurred during login",
+        snackPosition: SnackPosition.BOTTOM,
+      );
+    }
+    }
 
     
   }
