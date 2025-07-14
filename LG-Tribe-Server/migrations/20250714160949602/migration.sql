385d4b8aa7a765be34d1ee5698c6179e3218ae86
Pak_Man926 <davidkamau926@gmail.com>
2025-07-14 19:11:19 +0300

    Auto commit

---
+++ LG-Tribe-Server/migrations/20250714160949602/migration.sql
@@ -0,0 +1,21 @@
+BEGIN;
+
+
+--
+-- MIGRATION VERSION FOR lg_tribe
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('lg_tribe', '20250714160949602', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20250714160949602', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20240516151843329', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20240516151843329', "timestamp" = now();
+
+
+COMMIT;
