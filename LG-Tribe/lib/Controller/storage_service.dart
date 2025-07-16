4cfe2846c9689fdd70bb519a058a0d870b7afa1b
Pak_Man926 <davidkamau926@gmail.com>
2025-07-16 23:58:58 +0300

    Auto commit

--- LG-Tribe/lib/Controller/storage_service.dart
+++ LG-Tribe/lib/Controller/storage_service.dart
@@ -2,6 +2,6 @@ class StorageService
 {
     static Future<void> saveLoginData({required int contacts, required String authLevel, required String Country}) async
     {
-        final prefs = await SharedPreferences.
+        final prefs = await SharedPreferences.getInstance
     }
 }
