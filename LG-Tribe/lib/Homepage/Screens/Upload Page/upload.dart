923abf93cca25ddf745c75d13158e635fc770af4
Pak_Man926 <davidkamau926@gmail.com>
2025-07-23 23:45:20 +0300

    Auto commit

--- LG-Tribe/lib/Homepage/Screens/Upload Page/upload.dart
+++ LG-Tribe/lib/Homepage/Screens/Upload Page/upload.dart
@@ -1,4 +1,23 @@
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 
-class Upload
+class UploadImageScreen extends StatelessWidget
+{
+  const UploadImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Uploaded Image"),
+        backgroundColor: Color(0xFFA50034),
+      ),
+      body: Center(
+        child: Text(
+          "This is the screen for the uploaded image.",
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
