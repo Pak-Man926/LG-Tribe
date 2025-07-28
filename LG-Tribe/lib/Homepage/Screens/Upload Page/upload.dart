4887add0dc4e152fae6934d375835eee2f6db890
Pak_Man926 <davidkamau926@gmail.com>
2025-07-28 13:45:06 +0300

    Auto commit

--- LG-Tribe/lib/Homepage/Screens/Upload Page/upload.dart
+++ LG-Tribe/lib/Homepage/Screens/Upload Page/upload.dart
@@ -9,6 +9,7 @@ class UploadImageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -20,17 +21,15 @@ class UploadImageScreen extends StatelessWidget {
             style: TextStyle(fontWeight: FontWeight.w600, fontSize: 12)),
       ),
       body: Container(
-                  width: double.infinity,
-                  height: screenHeight * 0.5,
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.file(file)
-                  ),
-                ),
+        width: double.infinity,
+        height: screenHeight * 0.5,
+        decoration: BoxDecoration(
+          color: Colors.black12,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: ClipRRect(
+            borderRadius: BorderRadius.circular(10), child: Image.file(file)),
+      ),
     );
   }
 }
