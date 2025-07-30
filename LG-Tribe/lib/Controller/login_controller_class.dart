9a3e0f1d67094a028ad6afd707bebd234c5dbeb9
Pak_Man926 <davidkamau926@gmail.com>
2025-07-30 10:07:53 +0300

    Auto commit

--- LG-Tribe/lib/Controller/login_controller_class.dart
+++ LG-Tribe/lib/Controller/login_controller_class.dart
@@ -136,3 +136,13 @@ class LoginController extends GetxController {
   
   bool get isLoggedIn => loggedInUser.value != null;
 }
+// Method to get user details
+  Map<String, dynamic> getUserDetails() {
+    return {
+      'firstName': loggedInUser.value?.firstName ?? '',
+      'lastName': loggedInUser.value?.lastName ?? '',
+      'contacts': loggedInUser.value?.contacts ?? '',
+      'email': loggedInUser.value?.email ?? '',
+    };
+  }
+}
