51c9db653de66491715a67fac34df55c7dee8600
Pak_Man926 <davidkamau926@gmail.com>
2025-08-01 13:06:58 +0300

    Auto commit

--- LG-Tribe/lib/Homepage/Screens/Profile Page/profile.dart
+++ LG-Tribe/lib/Homepage/Screens/Profile Page/profile.dart
@@ -2,9 +2,16 @@ import "package:flutter/material.dart";
 //import "package:lg_tribe/Homepage/homepage.dart";
 import "package:get/get.dart";
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({super.key});
 
+  @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
