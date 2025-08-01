4264170dfa7aef725c2414ce695d7ad839e3bb13
Pak_Man926 <davidkamau926@gmail.com>
2025-08-01 12:55:23 +0300

    Auto commit

--- LG-Tribe/lib/Controller/registration_controller_class.dart
+++ LG-Tribe/lib/Controller/registration_controller_class.dart
@@ -5,8 +5,6 @@ import "package:serverpod_flutter/serverpod_flutter.dart";
 
 late final Client client;
 class RegistrationController extends GetxController {
-  static var to;
-
   // Client instance for making API calls
   // This will be initialized in the onInit method  
   @override
